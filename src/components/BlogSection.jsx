4a744dda7a9f86ed1b819ec0d89cac92a9c5463a
destinybravos <destinybravos@gmail.com>
2023-10-11 17:18:26 +0100

    Added Search UI and Functionality to the react component

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,15 +1,18 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { FaMagnifyingGlass} from 'react-icons/fa6'
+
 const BlogSection = () => {
   const baseUrl =  'http://localhost/procity-portal';
   const [posts, setPost] = useState([]);
-  const [categories, setSetCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [activeCateory, setActiveCateory] = useState('all');
 
-  const fetchPost = async (category = 'all') => {
+  const fetchPost = async (category = 'all', searchValue = '') => {
     let formData = new FormData();
     formData.append('action', 'fetch_post')
     formData.append('category', category)
+    formData.append('search_value', searchValue)
 
     await axios.post(`${baseUrl}/server/manage_post.php`, formData)
     .then((response) => {
@@ -26,16 +29,35 @@ const BlogSection = () => {
 
     await axios.post(`${baseUrl}/server/fetch_categories.php`, formData)
     .then((response) => {
-      setSetCategories(response.data.categories);
+      setCategories(response.data.categories);
     })
     .catch((err) => {
       console.log(err.message);
     });
   }
 
-  const setActiveCaategory = (category) => {
+  const fetchByCategory = (category) => {
     setActiveCateory(category);
     fetchPost(category);
+  }
+
+  const searchPost = (input_value) => {
+    fetchPost(activeCateory, input_value);
+  }
+
+  const submitSearchForm = async (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+    formData.append('action', 'fetch_post')
+    formData.append('category', activeCateory)
+
+    await axios.post(`${baseUrl}/server/manage_post.php`, formData)
+    .then((response) => {
+      setPost(response.data.posts);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
   }
   
   useEffect(() => {
@@ -52,18 +74,29 @@ const BlogSection = () => {
               <h2 className="mb-2">
                   Blog Categories
               </h2>
-              <div id="div_cats" className="flex gap-2 overflow-x-auto w-full">
-                {/* Default Button for All categories */}
-                <button onClick={() => setActiveCaategory('all')} className={`hover:bg-primary hover:text-white ${(activeCateory == 'all') ? 'bg-primary text-white' : 'border-primary border text-primary'} py-1 px-3 text-xs rounded-md italic`}>
-                    All
-                </button>
-                {/* Loop throgh all available categories */}
-                {categories && categories.map((cat) => (
-                  <button key={cat.id} onClick={() => setActiveCaategory(cat.category)} className={`hover:bg-primary hover:text-white ${activeCateory == cat.category ? 'bg-primary text-white' : 'border-primary border text-primary'} py-1 px-3 text-xs rounded-md italic`}>
-                      {cat.category}
+              <section className="flex justify-between">
+                {/* Categorries Tab Pills */}
+                <div id="div_cats" className="flex gap-2 overflow-x-auto w-full">
+                  {/* Default Button for All categories */}
+                  <button onClick={() => fetchByCategory('all')} className={`hover:bg-primary hover:text-white ${(activeCateory == 'all') ? 'bg-primary text-white' : 'border-primary border text-primary'} py-1 px-3 text-xs rounded-md italic`}>
+                      All
                   </button>
-                ))}
-              </div>
+                  {/* Loop throgh all available categories */}
+                  {categories && categories.map((cat) => (
+                    <button key={cat.id} onClick={() => fetchByCategory(cat.category)} className={`hover:bg-primary hover:text-white ${activeCateory == cat.category ? 'bg-primary text-white' : 'border-primary border text-primary'} py-1 px-3 text-xs rounded-md italic`}>
+                        {cat.category}
+                    </button>
+                  ))}
+                </div>
+
+                {/* Search Input */}
+                <form className="flex items-center border rounded-lg" onSubmit={(e) => submitSearchForm(e)}>
+                    <input onKeyUp={(e) => searchPost(e.target.value)} type="search" name="search_value" id="search" placeholder="Search Post" className="flex-grow rounded-lg px-3 py-1 focus:outline-none" />
+                    <button className="py-1 px-2">
+                      <FaMagnifyingGlass />
+                    </button>
+                </form>
+              </section>
 
               {/* <!-- display all fetured posts --> */}
               <div id="featured_posts" className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-20">
